7c1ff1bcb8f6b1e5955d16d72155f7d495817f80
Beorlor <jbdurand2000@gmail.com>
2023-10-18 11:15:34 +0200

    starting

--- main.c
+++ main.c
@@ -1,11 +1,8 @@
 #include "ft.h"
 
-If no parameters are specified, the program must not display anything and give the
-prompt back.
-
 int	main(int argc, char *argv[])
 {
 	if (argc < 2)
 		return (1);
-	
+
 }
